cf67d6878ac290322e76b24442a0018d26e4e17e
Andrew Snodgrass <pointlander@gmail.com>
2023-11-18 06:23:09 -0600

    Refactor

--- main.go
+++ main.go
@@ -10,46 +10,54 @@ import (
 	"math/rand"
 )
 
+const (
+	// Width is the width of the model
+	Width = 4
+)
+
+func neuron1(seed int64, id int, in chan [4]float32, out [3]chan float32) {
+	rng := rand.New(rand.NewSource(seed))
+	weights := NewMatrix(0, 4, 1)
+	bias := NewMatrix(0, 1, 1)
+	factor := math.Sqrt(2.0 / float64(4))
+	for i := 0; i < 4; i++ {
+		weights.Data = append(weights.Data, float32(rng.NormFloat64()*factor))
+	}
+	bias.Data = append(bias.Data, 0)
+	for input := range in {
+		i := NewMatrix(0, 4, 1)
+		i.Data = append(i.Data, input[:]...)
+		output := Step(Add(MulT(weights, i), bias))
+		for j := range out {
+			out[j] <- output.Data[0]
+		}
+	}
+}
+
+func neuron2(seed int64, id int, in [Width]chan float32, out chan float32) {
+	rng := rand.New(rand.NewSource(seed))
+	weights := NewMatrix(0, Width, 1)
+	bias := NewMatrix(0, 1, 1)
+	factor := math.Sqrt(2.0 / float64(Width))
+	for i := 0; i < Width; i++ {
+		weights.Data = append(weights.Data, float32(rng.NormFloat64()*factor))
+	}
+	bias.Data = append(bias.Data, 0)
+	for {
+		i := NewMatrix(0, Width, 1)
+		for _, in := range in {
+			i.Data = append(i.Data, <-in)
+		}
+		output := Add(MulT(weights, i), bias)
+		out <- output.Data[0]
+	}
+}
+
 func main() {
 	rng := rand.New(rand.NewSource(1))
-	neuron1 := func(seed int64, id int, in chan [4]float32, out [3]chan float32) {
-		rng := rand.New(rand.NewSource(seed))
-		weights := NewMatrix(0, 4, 1)
-		bias := NewMatrix(0, 1, 1)
-		factor := math.Sqrt(2.0 / float64(4))
-		for i := 0; i < 4; i++ {
-			weights.Data = append(weights.Data, float32(rng.NormFloat64()*factor))
-		}
-		bias.Data = append(bias.Data, 0)
-		for input := range in {
-			i := NewMatrix(0, 4, 1)
-			i.Data = append(i.Data, input[:]...)
-			output := Step(Add(MulT(weights, i), bias))
-			for j := range out {
-				out[j] <- output.Data[0]
-			}
-		}
-	}
-	neuron2 := func(seed int64, id int, in [4]chan float32, out chan float32) {
-		rng := rand.New(rand.NewSource(seed))
-		weights := NewMatrix(0, 4, 1)
-		bias := NewMatrix(0, 1, 1)
-		factor := math.Sqrt(2.0 / float64(4))
-		for i := 0; i < 4; i++ {
-			weights.Data = append(weights.Data, float32(rng.NormFloat64()*factor))
-		}
-		bias.Data = append(bias.Data, 0)
-		for {
-			i := NewMatrix(0, 4, 1)
-			for _, in := range in {
-				i.Data = append(i.Data, <-in)
-			}
-			output := Add(MulT(weights, i), bias)
-			out <- output.Data[0]
-		}
-	}
-	input := make([]chan [4]float32, 4)
-	output := make([]chan float32, 3*4)
+
+	input := make([]chan [4]float32, Width)
+	output := make([]chan float32, Width*3)
 	for i := range input {
 		input[i] = make(chan [4]float32, 8)
 	}
@@ -60,15 +68,16 @@ func main() {
 	for i := range top {
 		top[i] = make(chan float32, 8)
 	}
+
 	id := 0
-	for i := 0; i < 4; i++ {
-		go neuron1(rng.Int63(), id, input[i], [3]chan float32{output[i], output[i+4], output[i+8]})
+	for i := 0; i < Width; i++ {
+		go neuron1(rng.Int63(), id, input[i], [3]chan float32{output[i], output[i+Width], output[i+2*Width]})
 		id++
 	}
 	for i := 0; i < 3; i++ {
-		input := [4]chan float32{}
+		input := [Width]chan float32{}
 		for j := range input {
-			input[j] = output[4*i+j]
+			input[j] = output[Width*i+j]
 		}
 		go neuron2(rng.Int63(), id, input, top[i])
 		id++
