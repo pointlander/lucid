9400b2a727e5cbd091453d84a761771198576065
Andrew Snodgrass <pointlander@gmail.com>
2023-11-20 17:33:17 -0600

    Calculate correct predictions

--- main.go
+++ main.go
@@ -8,13 +8,17 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/pointlander/datum/iris"
 )
 
 const (
 	// Width is the width of the model
-	Width = 4
+	Width = 16
 	// Window is the size of the window
 	Window = 16
 )
@@ -26,9 +30,19 @@ type Inputs struct {
 	Epoch  int64
 }
 
-func neuron1(seed int64, id int, in <-chan Inputs, out [3]chan<- Input, done chan []Loss) {
+func neuron1(seed int64, id int, in <-chan Inputs, out [3]chan<- Input, done chan []Loss,
+	fini chan bool, dump chan Multi) {
 	//rng := rand.New(rand.NewSource(seed))
+	lock := sync.Mutex{}
+	lock.Lock()
 	multi := NewMulti(5)
+	lock.Unlock()
+	go func() {
+		<-fini
+		lock.Lock()
+		dump <- multi
+		lock.Unlock()
+	}()
 	for input := range in {
 		seeds := rand.New(rand.NewSource(input.Epoch))
 	sample:
@@ -45,7 +59,9 @@ func neuron1(seed int64, id int, in <-chan Inputs, out [3]chan<- Input, done cha
 						vars[k][j] = samples[k]
 					}
 				}
+				lock.Lock()
 				multi = Factor(vars, false)
+				lock.Unlock()
 				break sample
 			default:
 				epoch := seeds.Int63()
@@ -75,6 +91,7 @@ func neuron1(seed int64, id int, in <-chan Inputs, out [3]chan<- Input, done cha
 			}
 		}
 	}
+	dump <- multi
 }
 
 // Input is the input into the second layer
@@ -84,23 +101,35 @@ type Input struct {
 	Epoch  int64
 }
 
-func neuron2(seed int64, id int, in [Width]<-chan Input, out chan<- Input, done chan []Loss) {
+func neuron2(seed int64, id int, in [Width]<-chan Input, out chan<- Input, done chan []Loss,
+	fini chan bool, dump chan Multi) {
 	//rng := rand.New(rand.NewSource(seed))
+	lock := sync.Mutex{}
+	lock.Lock()
 	multi := NewMulti(Width + 1)
+	lock.Unlock()
+	go func() {
+		<-fini
+		lock.Lock()
+		dump <- multi
+		lock.Unlock()
+	}()
 	for {
 		select {
 		case losses := <-done:
-			vars := make([][]float32, 4+1)
+			vars := make([][]float32, Width+1)
 			for i := range vars {
 				vars[i] = make([]float32, Window)
 			}
 			for j := 0; j < Window; j++ {
 				samples := multi.Sample(rand.New(rand.NewSource(losses[j].Epoch + int64(id))))
-				for k := 0; k < 4+1; k++ {
+				for k := 0; k < Width+1; k++ {
 					vars[k][j] = samples[k]
 				}
 			}
+			lock.Lock()
 			multi = Factor(vars, false)
+			lock.Unlock()
 		default:
 			inputs := make([]Matrix, 3)
 			for i := range inputs {
@@ -109,11 +138,13 @@ func neuron2(seed int64, id int, in [Width]<-chan Input, out chan<- Input, done 
 			labels := []int{}
 			var epoch = int64(0)
 			for _, in := range in {
-				value := <-in
-				labels = value.Labels
-				epoch = value.Epoch
-				for k, v := range value.Input {
-					inputs[k].Data = append(inputs[k].Data, v)
+				select {
+				case value := <-in:
+					labels = value.Labels
+					epoch = value.Epoch
+					for k, v := range value.Input {
+						inputs[k].Data = append(inputs[k].Data, v)
+					}
 				}
 			}
 			weights := NewMatrix(0, Width, 1)
@@ -168,10 +199,27 @@ func main() {
 	for i := range done1 {
 		done1[i] = make(chan []Loss, 8)
 	}
+	fini := make([]chan bool, Width)
+	for i := range fini {
+		fini[i] = make(chan bool, 8)
+	}
+	fini1 := make([]chan bool, 3)
+	for i := range fini1 {
+		fini1[i] = make(chan bool, 8)
+	}
+	dump := make([]chan Multi, Width)
+	for i := range dump {
+		dump[i] = make(chan Multi, 8)
+	}
+	dump1 := make([]chan Multi, 3)
+	for i := range dump1 {
+		dump1[i] = make(chan Multi, 8)
+	}
 
 	id := 0
 	for i := 0; i < Width; i++ {
-		go neuron1(rng.Int63(), id, input[i], [3]chan<- Input{output[i], output[i+Width], output[i+2*Width]}, done[i])
+		go neuron1(rng.Int63(), id, input[i], [3]chan<- Input{output[i], output[i+Width],
+			output[i+2*Width]}, done[i], fini[i], dump[i])
 		id++
 	}
 	for i := 0; i < 3; i++ {
@@ -179,7 +227,7 @@ func main() {
 		for j := range input {
 			input[j] = output[Width*i+j]
 		}
-		go neuron2(rng.Int63(), id, input, top[i], done1[i])
+		go neuron2(rng.Int63(), id, input, top[i], done1[i], fini1[i], dump1[i])
 		id++
 	}
 
@@ -204,7 +252,13 @@ func main() {
 		losses[i].Loss = math.MaxFloat32
 	}
 	count := 0
-	for epoch := 1; epoch < 256; epoch++ {
+	c, work := make(chan os.Signal), true
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		work = false
+	}()
+	for epoch := 1; epoch < 256 && work; epoch++ {
 		in := Inputs{
 			Epoch:  int64(rng.Int31()),
 			Labels: make([]int, 3),
@@ -223,7 +277,7 @@ func main() {
 		fmt.Println(epoch)
 
 	search:
-		for {
+		for work {
 			outputs := make([]Matrix, 3)
 			for i := range outputs {
 				outputs[i] = NewMatrix(0, 3, 1)
@@ -275,4 +329,67 @@ func main() {
 			}
 		}
 	}
+
+	id = 0
+	weights := NewMatrix(0, 4, Width)
+	bias := NewMatrix(0, 1, Width)
+	for i, value := range dump {
+		fini[i] <- true
+		multi := <-value
+		samples := multi.U //multi.Sample(rand.New(rand.NewSource(losses[0].Epoch + int64(id))))
+		index := 0
+		for i := 0; i < 4; i++ {
+			weights.Data = append(weights.Data, samples[index])
+			index++
+		}
+		bias.Data = append(bias.Data, samples[index])
+		id++
+	}
+	weights1 := NewMatrix(0, Width, 3)
+	bias1 := NewMatrix(0, 1, 3)
+	for i, value := range dump1 {
+		fini1[i] <- true
+		multi := <-value
+		samples := multi.U //multi.Sample(rand.New(rand.NewSource(losses[0].Epoch + int64(id))))
+		index := 0
+		for i := 0; i < Width; i++ {
+			weights1.Data = append(weights1.Data, samples[index])
+			index++
+		}
+		bias1.Data = append(bias1.Data, samples[index])
+		id++
+	}
+
+	correct := 0
+	loss := 0.0
+	for _, fisher := range data.Fisher {
+		input := NewMatrix(0, 4, 1)
+		for _, v := range fisher.Measures {
+			input.Data = append(input.Data, float32(v))
+		}
+
+		output := Step(Add(MulT(weights, input), bias))
+		output = TaylorSoftmax(Add(MulT(weights1, output), bias1))
+		max, index := float32(0.0), 0
+		for i, value := range output.Data {
+			v := float32(value)
+			if v > max {
+				max, index = v, i
+			}
+		}
+		fmt.Println(index, max)
+		if index == iris.Labels[fisher.Label] {
+			correct++
+		}
+
+		expected := make([]float32, 3)
+		expected[iris.Labels[fisher.Label]] = 1
+
+		for i, v := range output.Data {
+			diff := float64(float32(v) - expected[i])
+			loss += diff * diff
+		}
+	}
+	fmt.Println("correct", correct, float64(correct)/150)
+	fmt.Println("loss", loss)
 }
