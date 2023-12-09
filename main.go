41c0f79e7fa2cccfaecc9d69696f0d589d951003
Andrew Snodgrass <pointlander@gmail.com>
2023-12-08 21:24:34 -0600

    WIP mark3

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/pointlander/lucid/mark1"
 	"github.com/pointlander/lucid/mark2"
+	"github.com/pointlander/lucid/mark3"
 )
 
 var (
@@ -16,6 +17,8 @@ var (
 	FlagMark1 = flag.Bool("mark1", false, "mark1 model")
 	// FlagMark2 is the mark2 model
 	FlagMark2 = flag.Bool("mark2", false, "mark2 model")
+	// FlagMark3 is the mark3 model
+	FlagMark3 = flag.Bool("mark3", false, "mark3 model")
 )
 
 func main() {
@@ -26,6 +29,9 @@ func main() {
 		return
 	} else if *FlagMark2 {
 		mark2.Mark2()
+		return
+	} else if *FlagMark3 {
+		mark3.Mark3()
 		return
 	}
 }
---
+++ mark3/model.go
@@ -0,0 +1,14 @@
+// Copyright 2023 The Lucid Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package mark3
+
+import (
+	. "github.com/pointlander/lucid/matrix"
+)
+
+func Mark3() {
+	a := NewMatrix(0, 8, 8)
+	_ = a
+}
